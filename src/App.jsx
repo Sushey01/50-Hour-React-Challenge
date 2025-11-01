bba18135bbbb02b65a41fc8237be30bf6d619174
Sushey01 <susmagar012@gmail.com>
2025-11-01 17:17:22 +0545

    Day1-IndentationErrorSucks

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Greet from "./components/Greet"
+
+const App = () =>{
+  return <div><Greet/></div>
+
+}
+
+export default App
---
+++ src/components/Greet.jsx
@@ -0,0 +1,5 @@
+const Greet = () =>{
+  return <h1>Hello Youths!</h1>
+}
+
+export default Greet;
