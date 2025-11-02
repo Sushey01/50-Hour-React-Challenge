3cb854f1db8eab9c597d8b81c8b0c88678ae7fbc
Sushey01 <susmagar012@gmail.com>
2025-11-02 23:01:45 +0545

    Day2-JSXRules

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import Footer from "./components/Footer"
 import Greet from "./components/Greet"
 import Header from "./components/Header";
+import JSXRules from "./components/JSXRules";
 import MainContent from "./components/MainContent";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () =>{
   return (
     <section id="section">
-      <div>
+      {/* <div>
         <h1>My Website</h1>
         <article>
           <h1>Welcome to React</h1>
           <p className="text">Paragraph Content </p>
         </article>
-      </div>
+      </div> */}
+      <WelcomeMessage/>
+      <JSXRules/>
     </section>
   );
 
