c00e7501356fce1be9b19ad3a5d1cd1c36455c76
Sushey01 <susmagar012@gmail.com>
2025-11-08 23:26:01 +0545

    Day7Self-doig-challenege

--- src/components/Challenges1/Todo.jsx
+++ src/components/Challenges1/Todo.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Todo = () => {
-    
+    const [todos, setTodos] = useState()
+    const [input, setInput] = useState("")
   return (
     <div>
-      
+      <h1>Type your todo here:</h1>
+      <input 
+      type="text"
+      value={input}
+      onChange={(e)=>setInput(e.target.value)}
+      />
     </div>
   )
 }
