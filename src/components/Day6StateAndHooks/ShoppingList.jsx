9b759f353e287f7aa5f1466932492cbc1453302b
Sushey01 <susmagar012@gmail.com>
2025-11-11 23:38:07 +0545

    Day7-continuing-state-challenge

---
+++ src/components/Day6StateAndHooks/ShoppingList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShoppingList = () => {
+  return (
+    <div>
+      <h1>DAY7 CONTINUING THE STATE CHALLENGE HAHA!</h1>
+    </div>
+  )
+}
+
+export default ShoppingList
